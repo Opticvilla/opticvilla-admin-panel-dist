c310f15d0dd3f3ff7cffc5b85a1a9aba5d40082e
arjunlohera <loheraarjun@gmail.com>
2025-07-31 01:03:50 +0530

    update

---
+++ admin-panel/browser/chunk-6YKWE4IU.js
@@ -0,0 +1,42 @@
+var o=`
+    <svg xmlns="http://www.w3.org/2000/svg" 
+        class="w-5 h-5" 
+        viewBox="0 0 20 20" 
+        fill="currentColor">
+    <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+    <path fill-rule="evenodd" 
+            d="M5 13.5V16h2.5l8.086-8.086-2.5-2.5L5 13.5zM3 17a1 1 0 001 1h3.5a1 1 0 00.707-.293l9.5-9.5a3 3 0 000-4.242l-1.5-1.5a3 3 0 00-4.242 0l-9.5 9.5A1 1 0 003 13.5V17z" 
+            clip-rule="evenodd" />
+    </svg>
+`,l=`
+<svg class="w-5 h-5 shrink-0" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+    <path d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-10 0h12" />
+</svg>
+`,s=`
+<svg xmlns="http://www.w3.org/2000/svg"
+     class="w-5 h-5"
+     fill="none"
+     viewBox="0 0 24 24"
+     stroke="currentColor"
+     stroke-width="2">
+  <path stroke-linecap="round" stroke-linejoin="round"
+        d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 114 0m-4 0a2 2 0 104 0M9 10h6M9 14h6" />
+</svg>
+
+`,t=`
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke="currentColor"
+  class="w-5 h-5"
+>
+  <path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6M9 7h6m2 0a1 1 0 001-1V5a1 1 0 00-1-1h-3.586a1 1 0 01-.707-.293l-.414-.414A1 1 0 0012.586 3H11.414a1 1 0 00-.707.293l-.414.414A1 1 0 019.586 4H6a1 1 0 00-1 1v1a1 1 0 001 1h12z"
+  />
+</svg>
+
+`;export{o as a,l as b,s as c,t as d};
